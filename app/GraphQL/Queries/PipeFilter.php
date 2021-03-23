61f51ecb63e682af3ce3b8ea93784f3fb8f404d0
ismaail <contact@ismaail.com>
2021-03-23 22:22:00 +0100

    Add PipeFilter Trait

---
+++ app/GraphQL/Queries/PipeFilter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
+/**
+ * Trait PipeFilter
+ * @package App\GraphQL\Queries
+ */
+trait PipeFilter
+{
+    /**
+     * @param \Illuminate\Database\Query\Builder $query
+     * @param string $name
+     * @param array $args
+     */
+    private function pipeFilterQuery(QueryBuilder $query, string $name, array $args, callable $callback = null): void
+    {
+        $value = $args[$name] ?? null;
+
+        if (null === $value) {
+            return;
+        }
+
+        if (null !== $callback) {
+            $callback();
+            return;
+        }
+
+        $query->where($name, $value);
+    }
+}
