e208b2836289ae32852120f3053610e1dbb7d25d
ismaail <contact@ismaail.com>
2021-03-23 22:22:44 +0100

    Add GraphQL ProjectType & UserType classes

---
+++ app/GraphQL/Types/ProjectType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\GraphQL\Types;
+
+use App\Models\Project;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Type as GraphqlType;
+
+/**
+ * Class ProjectType
+ * @package App\Graphql\Types
+ */
+class ProjectType extends GraphqlType
+{
+    /**
+     * @var array
+     */
+    protected $attributes = [
+        'name' => 'Project',
+        'model' => Project::class,
+    ];
+
+    /**
+     * @return array
+     */
+    public function fields(): array
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+            ],
+            'name' => [
+                'type' => Type::nonNull(Type::string()),
+            ],
+            'archived' => [
+                'type' => Type::nonNull(Type::boolean()),
+            ],
+            'owner' => [
+                'type' => GraphQL::Type('User'),
+            ]
+        ];
+    }
+}
