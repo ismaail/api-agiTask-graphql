62eb38f7c6f9efb8b2bf2b59fb6ef1684b6301ca
ismaail <contact@ismaail.com>
2021-03-23 02:23:49 +0100

    Update DatabaseSeeder: truncate all table and use ProjectFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,35 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Seeder;
 
+/**
+ * Class DatabaseSeeder
+ * @package Database\Seeders
+ */
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->truncateAllTable();
+
+        $users = \App\Models\User::factory(1)->create();
+
+        Project::factory(3)->create([
+            'owner_id' => $users[0]->id,
+        ]);
+    }
+
+    /**
+     * Truncate all the tables.
+     */
+    private function truncateAllTable(): void
+    {
+        \DB::statement('TRUNCATE TABLE projects CASCADE');
+        \DB::statement('TRUNCATE TABLE users CASCADE');
     }
 }
