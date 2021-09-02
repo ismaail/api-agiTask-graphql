e6e2ed9e24a8c4a9e1f69bebffe93b13813409b1
ismaail <contact@ismaail.com>
2021-09-02 22:03:18 +0100

    Update User Model & add Board & BoardMember Models

---
+++ app/Models/Board.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * Class Board
+ * @package App\Models
+ */
+class Board extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = 'boards';
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'atchived' => 'boolean',
+    ];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('board_member', fn(Builder $builder) => $builder->whereHas(
+            'members',
+            fn(Builder $builder) => $builder->where('id', Auth::user()->id)
+        ));
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'board_member', 'board_id', 'user_id')
+            ->as('membership')
+            ->withPivot(['relation'])
+            ->using(BoardMember::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function owner()
+    {
+        return $this->belongsToMany(User::class, 'board_member', 'board_id', 'user_id')
+            ->where('relation', BoardMember::RELATION_OWNER)
+            ->using(BoardMember::class);
+    }
+}
