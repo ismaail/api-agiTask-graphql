d39a1e1939b258f612671c0cbdb0f4df230eba0c
ismaail <contact@ismaail.com>
2021-09-15 23:01:55 +0100

    Add Bucket Model with Factory

---
+++ app/Models/Bucket.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * Class Bucket
+ * @package App\Models
+ */
+class Bucket extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = 'buckets';
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'archived',
+        'is_sprint',
+        'starts_at',
+        'ends_at',
+    ];
+
+    /**
+     * @var array<string,string>
+     */
+    protected $casts = [
+        'archived' => 'boolean',
+        'is_sprint' => 'boolean',
+        'starts_at' => 'immutable_datetime',
+        'ends_at' => 'immutable_datetime',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $dates = [
+        'starts_at',
+        'ends_at',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function board(): BelongsTo
+    {
+        return $this->belongsTo(Board::class, 'board_id', 'id');
+    }
+}
---
+++ database/factories/BucketFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bucket;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * Class BucketFactory
+ * @package Database\Factories
+ */
+class BucketFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Bucket::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(),
+        ];
+    }
+}
