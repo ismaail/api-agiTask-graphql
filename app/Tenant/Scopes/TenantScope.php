033b7eddcda3e1579b73c7db08d2636887664bdb
ismaail <contact@ismaail.com>
2021-09-16 05:56:30 +0100

    Add TenantScope Class & HasTenant Trait

---
+++ app/Tenant/Scopes/TenantScope.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tenant\Scopes;
+
+use App\Tenant\TenantManager;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Class TenantScope
+ * @package App\Models
+ *
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+ */
+class TenantScope implements Scope
+{
+    /**
+     * TenantScope constructor.
+     *
+     * @param \App\Tenant\TenantManager $tenantManager
+     */
+    public function __construct(private TenantManager $tenantManager)
+    {
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     * @param \Illuminate\Database\Eloquent\Model $model
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        $builder->where(
+            $this->tenantManager->getPrimaryKey(),
+            $this->tenantManager->getTenant()->id
+        );
+    }
+}
