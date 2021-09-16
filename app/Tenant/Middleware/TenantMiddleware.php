316e912254d342492e9ce1e82ccb0726366e6e32
ismaail <contact@ismaail.com>
2021-09-16 05:55:52 +0100

    Add TenantMiddleware

---
+++ app/Tenant/Middleware/TenantMiddleware.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Tenant\Middleware;
+
+use Closure;
+use App\Models\Board;
+use Illuminate\Http\Request;
+use App\Tenant\TenantManager;
+use App\Exceptions\TenantException;
+
+/**
+ * Class TenantMiddleware
+ * @package App\Http\Middleware
+ */
+class TenantMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $tenant = $this->resolveTenant($request->header('x-tenant-id'));
+
+        /** @var \App\Tenant\TenantManager $tenantManager */
+        $tenantManager = app(TenantManager::class);
+        $tenantManager->setTenant($tenant);
+
+        return $next($request);
+    }
+
+    /**
+     * @param int|null $tenantId
+     *
+     * @return \App\Models\Board
+     *
+     * @throws \App\Exceptions\TenantException
+     */
+    private function resolveTenant(?int $tenantId): Board
+    {
+        $tenant = Board::find($tenantId);
+
+        if (! $tenant) {
+            throw new TenantException('No Tenant provided');
+        }
+
+        return $tenant;
+    }
+}
