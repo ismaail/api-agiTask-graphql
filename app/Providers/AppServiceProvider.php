060a0a92da542c309ccfe5e355419952a85b819e
ismaail <contact@ismaail.com>
2022-11-19 04:28:44 +0100

    Make Models 'Strict' for non 'production' environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class AppServiceProvider
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::shouldBeStrict('production' !== app()->environment());
     }
 }
